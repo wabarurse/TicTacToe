c6d426ff8c5ce82a0a225a8921d1ff1096610031
wqba <wabarurse@gmail.com>
2024-07-29 11:46:37 -0400

    Created SwiftUI project

---
+++ TicTacToeUI/TicTacToeUI/TicTacToeUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeUIApp.swift
+//  TicTacToeUI
+//
+//  Created by Richard Huang on 2024-07-29.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
