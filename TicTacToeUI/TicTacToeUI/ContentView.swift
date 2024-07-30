fc0ed1b34fd13e10a070837eef170ecb269f7788
wqba <wabarurse@gmail.com>
2024-07-30 14:55:21 -0400

    Player and random computer move

--- TicTacToeUI/TicTacToeUI/ContentView.swift
+++ TicTacToeUI/TicTacToeUI/ContentView.swift
@@ -7,15 +7,91 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible()) ]
+    
+    @State private var moves: [Move?] = Array(repeating: nil, count: 9)
+    @State private var boardDisabled = false
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { geometry in
+            VStack {
+                Spacer()
+                LazyVGrid(columns: columns, spacing: 5) {
+                    ForEach(0..<9) { i in
+                        ZStack {
+                            Circle()
+                                .foregroundColor(.red).opacity(0.5)
+                                .frame(width: geometry.size.width/3 - 20,
+                                       height: geometry.size.width/3 - 20)
+                            
+                            
+                            Image(systemName: moves[i]?.indicator ?? "")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                                .foregroundColor(.white)
+                            
+                        }
+                        .onTapGesture {
+                            if isSquareOccupied(in: moves, forIndex: i) { return }
+                            
+                            moves[i] = Move(player: .human, boardIndex: i)
+                            boardDisabled = true
+                            
+                            
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                let computerPosition = computerMove(in: moves)
+                                moves[computerPosition] = Move(player: .computer, boardIndex: computerPosition)
+                                boardDisabled = false
+
+
+                            }
+                            
+                        }
+                    }
+                    
+                }
+                Spacer()
+            }
+            .disabled(boardDisabled)
+            .padding()
+            
         }
-        .padding()
+    }
+    
+    func isSquareOccupied(in moves: [Move?], forIndex index: Int) -> Bool {
+        return moves.contains(where: {$0?.boardIndex == index})
+    }
+    
+    func computerMove(in moves: [Move?]) -> Int {
+        var movePosition = Int.random(in: 0..<9)
+        
+        while isSquareOccupied(in: moves, forIndex: movePosition) {
+            movePosition = Int.random(in: 0..<9)
+        }
+        
+        return movePosition
+        
+    }
+    
+}
+
+enum Player {
+    case human, computer
+}
+
+struct Move {
+    let player: Player
+    let boardIndex: Int
+    
+    var indicator: String {
+        return player == .human ? "xmark" : "circle"
     }
 }
 
