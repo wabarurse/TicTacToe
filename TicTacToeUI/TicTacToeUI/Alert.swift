744760e16b2dff765e45df9b6e370aad27247e60
wqba <wabarurse@gmail.com>
2024-08-05 16:04:41 -0400

    Project folder change

--- TicTacToeUI/TicTacToeUI/Alert.swift
+++ TicTacToeUI/TicTacToeUI/Alert.swift
@@ -1,8 +1,23 @@
 //
 //  Alert.swift
-//  TicTacToeUI
+//  TicTacToeUI2
 //
 //  Created by Richard Huang on 2024-08-05.
 //
 
-import Foundation
+import SwiftUI
+
+struct AlertItem: Identifiable {
+    let id = UUID()
+    var title: Text
+    var message: Text
+    var button: Text
+}
+
+struct AlertContent {
+    static let humanWin = AlertItem(title: Text("You won!"), message: Text("Thats impossible, what happened?!"), button: Text("Fix code"))
+    static let computerWin = AlertItem(title: Text("You lost"), message: Text("Lost to a comptuer, smh"), button: Text("Try again"))
+    static let draw = AlertItem(title: Text("Draw"), message: Text("Expected"), button: Text("Rematch"))
+
+
+}
