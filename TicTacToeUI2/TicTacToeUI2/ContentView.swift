b4fb9f04dd2aea1125e956e55fa92ebe50e4d8b7
wqba <wabarurse@gmail.com>
2024-08-05 14:56:08 -0400

    UI for tictactoe AI is finished, the computer never loses now. Need to refractor code and add pop up message when gaem is over.

--- TicTacToeUI2/TicTacToeUI2/ContentView.swift
+++ TicTacToeUI2/TicTacToeUI2/ContentView.swift
@@ -31,28 +31,59 @@ struct ContentView: View {
                     
                 }
                 .onTapGesture {
-                    
-                    
-                    
+
                     if(moves[i] == nil) {
                         moves[i] = Move(player: .human, index: i)
                         board[i] = "o"
                         numVacant -= 1
                     }
+                    printBoard()
+
                     
-//                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5)  {
-//                        print(computerMove())
-//                        let computerPosition = computerMove()
-//                        moves[computerPosition] = Move(player: .computer, index: computerPosition)
-//                        board[computerPosition] = "x"
-//                        numVacant -= 1
-//
-//                    }
+                    if(determineWinner() == "o") {
+                        print("Human win")
+                    }
+                    
+//                    computerMove()
+//                    printBoard()
+                    
+                    
+                    
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                        let computerPosition = computerMove()
+                        print(computerPosition)
+                        moves[computerPosition] = Move(player: .computer, index: computerPosition)
+                        board[computerPosition] = "x"
+                        numVacant -= 1
+                        printBoard()
+                        
+                        if(determineWinner() == "x") {
+                            print("Computer win")
+                        }
+                        
+
+
+                    }
+                    
+                    
+                    
+                    
                     
                 }
             }
         }
         .padding(20)
+    }
+    
+    func printBoard() {
+        for i in 0..<9 {
+            print(board[i], terminator: "");
+            
+            if((i + 1) % 3 == 0) {
+                print("\n");
+            }
+            
+        }
     }
     
     
@@ -62,28 +93,28 @@ struct ContentView: View {
         // horizontal
         for i in stride(from: 0, through: 6, by: 3) {
             if(board[i] == board[i + 1] && board[i] == board[i + 2] && board[i] != "_") {
-                winner = board[i];
+                winner = board[i]
             }
         }
         
         // vertical
         for i in stride(from: 0, through: 2, by: 1) {
             if(board[i] == board[i + 3] && board[i] == board[i + 6] && board[i] != "_") {
-                winner = board[i];
+                winner = board[i]
             }
         }
         
         //diagonal
         if(board[0] == board[4] && board[0] == board[8] && board[0] != "_") {
-            winner = board[0];
+            winner = board[0]
         } else if(board[2] == board[4] && board[2] == board[6] && board[2] != "_") {
-            winner = board[2];
+            winner = board[2]
         }
         
         
         //tie
         if(winner == "/" && numVacant == 0) {
-            winner = "t";
+            winner = "t"
         }
         
         
@@ -97,43 +128,43 @@ struct ContentView: View {
         
         if(winner != "/") {
             if(winner == "x") {
-                return 10 + numVacant;
-            } else if(winner == "x") {
-                return -10 - numVacant;
+                return 10 + numVacant
+            } else if(winner == "o") {
+                return -10 - numVacant
             } else {
                 return 0;
             }
         }
         
         if(isMaximizing) {
-            var bestScore: Int = -100;
+            var bestScore: Int = -100
             for i in 0..<9 {
                 if (board[i] == "_") {
                     
-                    board[i] = "x"; numVacant-=1;
+                    board[i] = "x"; numVacant-=1
                     
-                    let currScore: Int = minmax(isMaximizing: false);
+                    let currScore: Int = minmax(isMaximizing: false)
                     
-                    board[i] = "_"; numVacant+=1;
+                    board[i] = "_"; numVacant+=1
                     
-                    bestScore = max(currScore, bestScore);
+                    bestScore = max(currScore, bestScore)
                 }
             }
             
             return bestScore;
             
         } else {
-            var bestScore: Int = 100;
+            var bestScore: Int = 100
             for i in 0..<9 {
                 if (board[i] == "_") {
+                     
+                    board[i] = "o"; numVacant-=1
                     
-                    board[i] = "o"; numVacant-=1;
+                    let currScore: Int = minmax(isMaximizing: true)
                     
-                    let currScore: Int = minmax(isMaximizing: true);
+                    board[i] = "_"; numVacant+=1
                     
-                    board[i] = "_"; numVacant+=1;
-                    
-                    bestScore = min(currScore, bestScore);
+                    bestScore = min(currScore, bestScore)
                 }
             }
             
@@ -144,30 +175,45 @@ struct ContentView: View {
     }
     
     
-    func computerMove() -> Int {
-        var bestScore: Int = -100;
-        var coord: Int = -1;
+    func computerMove() -> Int{
+        var bestScore: Int = -100
+        var coord: Int = -1
         
         for i in 0..<9 {
             if (board[i] == "_") {
                 
-                board[i] = "x"; numVacant-=1;
+                board[i] = "x"; numVacant-=1
                 
-                let currScore: Int = minmax(isMaximizing: false);
+                let currScore: Int = minmax(isMaximizing: false)
                 
-                board[i] = "_"; numVacant+=1;
+                board[i] = "_"; numVacant+=1
 
                 if (currScore > bestScore) {
-                    bestScore = currScore;
-                    coord = i;
-                
+                    bestScore = currScore
+                    coord = i
+            
                 }
             }
         }
         
         return coord
-        
+                
     }
+    
+}
+
+func isSquareOccupied(in moves: [Move?], forIndex i: Int) -> Bool {
+    return moves.contains(where: {$0?.index == i})
+}
+
+func randComputerMove(in moves: [Move?]) -> Int {
+    var movePosition = Int.random(in: 0..<9)
+    
+    while isSquareOccupied(in: moves, forIndex: movePosition) {
+        movePosition = Int.random(in: 0..<9)
+    }
+    
+    return movePosition
     
 }
     
@@ -179,6 +225,10 @@ enum Player {
 struct Move {
     let player: Player
     let index: Int
+    
+//    var char: Character {
+//        return player == .human ? "o" : "x"
+//    }
     
     var icon: String {
         return player == .human ? "circle" : "xmark"
